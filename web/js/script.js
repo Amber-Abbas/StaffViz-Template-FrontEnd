c331292804f40910d40a941d6e8ad89201d96112
Amber Abbas. <a.abbas@crecentech.com>
2024-08-29 10:41:10 -0500

    work

--- web/js/script.js
+++ web/js/script.js
@@ -1,7 +1,7 @@
 
 var swiper = new Swiper(".mySwiper", {
   watchSlidesProgress: true,
-  slidesPerView: 6,
+  slidesPerView: 7,
   autoplay: {
     delay: 2, // Delay in ms between slides
     disableOnInteraction: false, // Continue autoplay after user interaction
