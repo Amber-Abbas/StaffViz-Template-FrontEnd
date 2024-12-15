b9065f2dc3b5566bf47f71ac7f53c11608b6b030
Amber-Abbas <amberbuttse22@gmail.com>
2024-12-15 17:01:07 +0500

    Saving local changes before rebase

--- web/js/script.js
+++ web/js/script.js
@@ -1,4 +1,3 @@
-
 var swiper = new Swiper(".mySwiper", {
   watchSlidesProgress: true,
   slidesPerView: 7,
@@ -8,23 +7,19 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-
-
-
 function set_error(id, error) {
-  // Set errors inside the tag of the element with the given id
   var element = document.getElementById(id);
-  var errorElement = element.querySelector('.form-error');
-  console.log('errorElement', errorElement)
+  var errorElement = element.querySelector(".form-error");
+  console.log("errorElement", errorElement);
   if (errorElement) {
     errorElement.innerHTML = error;
-    errorElement.style.color = 'red';
+    errorElement.style.color = "red";
   }
 }
 
 function validateForm(event) {
   event.preventDefault();
- let errors=[];
+  let errors = [];
   // Get the input elements by their ID
   var nameInput = document.getElementById("name");
   var emailInput = document.getElementById("email");
@@ -38,20 +33,21 @@ function validateForm(event) {
   } else {
     set_error("nameInput", ""); // Clear the error if valid
   }
-  console.log("emailInput",emailInput)
+  console.log("emailInput", emailInput);
   // Validate email input
-  console.log('emailInput.value', emailInput.value)
-  let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  console.log("emailInput.value", emailInput.value);
+  let validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if (emailInput.value.length < 3 || !emailInput.value.match(validRegex)) {
     set_error("email-box", "Invalid email address");
     // return false;
-
   } else {
     set_error("email", ""); // Clear the error if valid
   }
 
   // Validate password input
-  if (passInput.value.length < 6) { // Example validation: password should be at least 6 characters
+  if (passInput.value.length < 6) {
+    // Example validation: password should be at least 6 characters
     set_error("password-box", "Password should be at least 6 characters");
     return false;
   } else {
@@ -63,11 +59,9 @@ function validateForm(event) {
     set_error("confirm-password-box", "Passwords do not match");
     return false;
   } else {
-    set_error("confirm-password-box", ""); // Clear the error if valid
+    set_error("confirm-password-box", "");
   }
 
   // Allow form submission if all validations pass
   return true;
 }
-
-
